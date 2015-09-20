05ce5f6dd0225bd928a45467bcd920889683acbe
doudoupower <529452103@qq.com>
2015-09-21 02:01:21 +0800

    092111

--- 高级特性.py
+++ 高级特性.py
@@ -11,3 +11,19 @@ L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
 print(L[0:2])
 print((0, 1, 2, 3, 4, 5)[:3])
 print('ABCDEFG'[:3])
+
+# 迭代
+d = {'a': 1, 'b': 2, 'c': 3}
+for key in d:
+    print(key)
+    # 如果要迭代value，可以用for value in d.values()，如果要同时迭代key和value，可以用for k, v in d.items()
+
+for ch in 'ABC':
+    print(ch)
+
+    #下标循环迭代
+for i, value in enumerate(['A', 'B', 'C']):
+    print(i, value)
+
+for x, y in [(1, 1), (2, 4), (3, 9)]:
+    print(x, y)
